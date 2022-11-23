5f979eddb5d1e696938fa13b59a0abe5470536f4
Naimerith <naimerith89@gmail.com>
2022-11-23 11:21:38 -0500

    controlando el estado de los checkbox

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,24 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Checkbox = ({ state }) => {
+  const arrayCheckbox = [];
+  const [isChecked, setIsChecked] = useState(false);
+
+  const [select, setSelect] = useState([]);
+  const handleSelect = (e) => {
+    const valueCheckbox = e.target.value;
+    //console.log(valueCheckbox);
+    if (e.target.checked) {
+      arrayCheckbox.push(valueCheckbox);
+      console.log("agrega", arrayCheckbox);
+    } else {
+      arrayCheckbox.pop(valueCheckbox);
+      console.log("elimina", arrayCheckbox);
+    }
+  };
+  const handleSelectAll = (e) => {
+    setIsChecked(!isChecked);
+    /*const valueCheckbox = e.target.value;
+    arrayCheckbox.push(valueCheckbox);
+    setSelect(arrayCheckbox);*/
+  };
+
   return (
     <div>
-      <div>
-        <input id="selectAll" name="seleccionar todo" type="checkbox" />
-        SELECCIONAR TODO
-        {state.map((el, i) => {
-          return (
-            <div key={i}>
-              <input
-                type="checkbox"
-                value={el}
-                onChange={(e) => console.log(e.target.value)}
-              />
-              {el}
-            </div>
-          );
-        })}
-      </div>
+      <input
+        name="checkbox"
+        type="checkbox"
+        checked={isChecked}
+        onChange={handleSelectAll}
+      />
+      SELECCIONAR TODO
+      {state.map((el, i) => {
+        return (
+          <div key={i}>
+            <input
+              name="checkbox"
+              type="checkbox"
+              value={el}
+              onChange={handleSelect}
+            />
+            {el}
+          </div>
+        );
+      })}
     </div>
   );
 };
